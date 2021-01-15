f88148e4d65e0ceacf4110ec14cb456964a8cef8
Takato1114 <tk.umbrella6535@gmail.com>
2021-01-15 21:11:32 +0900

    Seccond Commit

---
+++ src/Chapter04.java
@@ -0,0 +1,15 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String str1;
+		str1 = "Takato";
+		System.out.println(str1);
+
+		str1="こんにちは";
+		System.out.println(str1);
+
+		final int number =7;
+		System.out.println(number);
+	}
+
+}
---
+++ src/Chapter05.java
@@ -0,0 +1,23 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+	long long1 =  1111111L;
+	double double1 = 1.11;
+	boolean boolean1 = true;
+
+	System.out.println(long1);
+	System.out.println(double1);
+	System.out.println(boolean1);
+
+	Float f = 1.11f;
+	Integer i =  111;
+	Character c = 'あ';
+
+	System.out.println(f);
+	System.out.println(i);
+	System.out.println(c);
+
+	int[] array = { 10, 20, 30, 40, 50 };
+	System.out.println(array[3]);
+	}
+}
